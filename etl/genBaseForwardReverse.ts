3f3efdd338189926a710415c26e7bef60ec1e7f6
Bryan Kok <bryan.wyern1@gmail.com>
2021-01-27 15:45:34 +0800

    Add function converting all kangxi radicals in a string to CJK characters, compensate for utfstring bug, determine base radicals by first adding all radicals and then removing all those which have decompositions
    Add utf to kangxi lookup table

--- etl/genBaseForwardReverse.ts
+++ etl/genBaseForwardReverse.ts
@@ -2,6 +2,9 @@ export function sum(a: number, b: number) {
   return a + b;
 }
 
+import fs from "fs";
+import path from "path";
+
 import {
   IDCSet,
   StrokePlaceholderSet,
@@ -30,6 +33,36 @@ import { writeJSON } from "./writer";
   const split = text.split("\n");
 }; */
 
+const kangxiToCJKMap = new Map<String, String>();
+{
+  const rawKangxiToCJK = fs
+    .readFileSync(path.join(__dirname, "utf-to-kangxi.csv"), "utf8")
+    .split("\n");
+  for (let entry of rawKangxiToCJK) {
+    let split = entry.split(",");
+    // @ts-ignore
+    const kangxi = String.fromCodePoint(Number(split[1], 10));
+    // @ts-ignore
+    const cjk = String.fromCodePoint(Number(split[0], 10));
+    kangxiToCJKMap.set(kangxi, cjk);
+  }
+}
+
+export const kangxiToCJK = (input: string): string => {
+  const output = [];
+  let currentChar;
+  for (let i = 0; i < utfstring.length(input); i++) {
+    // there is a bug in utfstring.charAt at the time of writing
+    currentChar = utfstring.fromCharCode(utfstring.charCodeAt(input, i));
+    if (kangxiToCJKMap.has(currentChar)) {
+      output.push(kangxiToCJKMap.get(currentChar));
+    } else {
+      output.push(currentChar);
+    }
+  }
+  return output.join("");
+};
+
 const processIDSText = (resolvedIDSData: string[][]) => {
   const forwardMap: any = {};
   const reverseMap: ReverseMap = {};
@@ -50,6 +83,9 @@ const processIDSText = (resolvedIDSData: string[][]) => {
     const reverseMapValue = [];
 
     for (let i = 2; i < entry.length; i++) {
+      // it does appear that kawabata and glyphwiki do not use kangxi radicals,
+      // where possible, but just in case
+      entry[i] = kangxiToCJK(entry[i]);
       const localeStart = /[[][A-Z]+[\]]/.exec(entry[i]);
       const str =
         localeStart !== null ? entry[i].substr(0, localeStart.index) : entry[i];
@@ -62,10 +98,15 @@ const processIDSText = (resolvedIDSData: string[][]) => {
 
       // for (let i = 0; i < str.length; i++) {
       for (let i = 0; i < utfstring.length(str); i++) {
-        // let radical = str.charAt(i);
-        let radical = utfstring.charAt(str, i);
+        // bug with utfstring
+        // let radical = utfstring.charAt(str, i);
+        let radical = utfstring.fromCharCode(utfstring.charCodeAt(str, i));
         if (IDCSet.has(radical)) continue;
         if (StrokePlaceholderSet.has(radical)) continue;
+        // ignore all ascii
+        if (radical.charCodeAt(0) < 127) continue;
+        // add everything 1st
+        baseRadicals.add(radical);
         if (radical === char) {
           /* e.g. U+27C28	𧰨	𧰨, there exists an entry where this radical
             can only be described by itself */
@@ -77,19 +118,26 @@ const processIDSText = (resolvedIDSData: string[][]) => {
           continue;
         }
 
-        // ignore all ascii
-        if (radical.charCodeAt(0) < 127) continue;
-        // add everything 1st
-        baseRadicals.add(radical);
         if (!(radical in forwardMap)) {
           forwardMap[radical] = new Set();
         }
         forwardMap[radical].add(char);
       }
     }
-    reverseMap[char] = { utf_code: entry[0], ids_strings: reverseMapValue };
+
+    // unihan-fetcher simply concats all the entries in the different files
+    // together, 𨘒 (U+28612) is both in
+    // MANUALLY_RESOLVED_ and MANUALLY_PARTIALLY_RESOLVED_
+    if (char in reverseMap) {
+      reverseMap[char].ids_strings = reverseMap[char].ids_strings.concat(
+        reverseMapValue
+      );
+    } else {
+      reverseMap[char] = { utf_code: entry[0], ids_strings: reverseMapValue };
+    }
   }
 
+  // .... and remove all characters that have a decomposition later
   for (let entry of resolvedIDSData) {
     const utfCp = entry[0];
     const char = entry[1];
