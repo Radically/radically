94c032e5bc63b69145dad9ed155cedec6ace53d4
Bryan Kok <bryan.wyern1@gmail.com>
2021-01-30 19:51:03 +0800

    Add ETL driver file

--- etl/driver.ts
+++ etl/driver.ts
@@ -1,12 +1,24 @@
 import genBaseForwardReverse from "./genBaseForwardReverse";
 import genStrokeCount from "./genStrokeCount";
 import genReadings from "./genReadings";
+import genVariants from "./genVariants";
+
+// https://stackoverflow.com/questions/23003252/window-performance-now-equivalent-in-nodejs
+function clock(start: any) {
+  if (!start) return process.hrtime();
+  var end = process.hrtime(start);
+  return Math.round(end[0] * 1000 + end[1] / 1000000);
+}
 
 const main = async () => {
+  const startTime = clock(null);
   console.log("ETL Start");
   await genBaseForwardReverse();
   await genStrokeCount();
   await genReadings();
+  await genVariants();
+  // @ts-ignore
+  console.log(`ETL End, ${clock(startTime) / 1000} s elapsed`);
 };
 
 if (require.main === module) {
