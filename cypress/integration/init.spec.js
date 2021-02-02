5bee9213108c0b355278fb661992f7492fd7e7e1
Bryan Kok <bryan.wyern1@gmail.com>
2021-02-03 03:37:40 +0800

    Test background change upon dark mode toggle

--- cypress/integration/init.spec.js
+++ cypress/integration/init.spec.js
@@ -16,4 +16,15 @@ describe("Cypress", () => {
 
     cy.get("#radical-frequency-checkbox").should("be.checked");
   });
+
+  it("dark mode working", () => {
+    cy.viewport("iphone-5", "landscape");
+    cy.visit("http://localhost:3000");
+    cy.get("#darkmode-toggle").click();
+    cy.get("#first-page-container").should(
+      "have.css",
+      "background-color",
+      "rgb(48, 48, 48)"
+    );
+  });
 });
