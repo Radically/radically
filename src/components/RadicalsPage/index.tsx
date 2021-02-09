5f107678f598e4aa1ed080e7456fa4daba1ea538
Bryan Kok <bryan.wyern1@gmail.com>
2021-02-10 00:39:40 +0800

    Mockup of resizable readings pane in the RadicalsPage

--- src/components/RadicalsPage/index.tsx
+++ src/components/RadicalsPage/index.tsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import styled from "styled-components";
 import { withTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
 import IconButton from "@material-ui/core/IconButton";
 import SearchIcon from "@material-ui/icons/Search";
-// import ResizePanel from "react-resize-panel";
+import Split from "react-split";
 
 import { heightPx } from "../OutputBar";
+
+import { SettingsContext } from "../../contexts/SettingsContextProvider";
 
 const RadicalsPageContainer = styled("div")`
   display: flex;
@@ -58,13 +62,71 @@ const SearchInput = withTheme(
   `
 );
 
+const StrokesRadicalsContainer = styled.div`
+  @media (orientation: portrait) {
+    border-bottom: 1px solid #909090;
+  }
+  // flex-grow: 1;
+`;
+
+const StrokesScrollContainer = styled.div`
+  // flex: 0.1;
+  width: 50px;
+  float: left;
+
+  background-color: green;
+  border-right: 1px solid #909090;
+
+  // do i really have to set a height here??
+  min-height: 100%;
+  height: 0px;
+  overflow-y: scroll;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const RadicalsScrollContainer = styled.div`
+  // flex: 0.1;
+  width: calc(100% - 51px);
+  float: right;
+
+  background-color: purple;
+
+  // do i really have to set a height here??
+  min-height: 100%;
+  height: 0px;
+  overflow-y: scroll;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const ReadingsScrollContainer = styled.div`
+  background-color: blue;
+  flex-grow: 1;
+
+  border-top: 1px solid #909090;
+`;
+const ls = [] as number[];
+{
+  for (let i = 1; i < 30; i++) {
+    ls.push(i);
+  }
+}
+
 function RadicalsPage() {
   const [input, setInput] = useState("");
+
+  const { darkMode } = useContext(SettingsContext);
+
+  const isLandscape = useMediaQuery("(orientation: landscape)");
+  const isSafari = navigator.vendor.includes("Apple");
 
   return (
     <RadicalsPageContainer id="radicals-page-container">
       <SearchContainer>
-        <SearchInput placeholder="食喜" />
+        <SearchInput placeholder="Find decompositions or variants" />
 
         <IconButton
           onClick={() => {
@@ -79,11 +141,41 @@ function RadicalsPage() {
         </IconButton>
       </SearchContainer>
 
-      <div style={{ backgroundColor: "green", flexGrow: 2 }}></div>
+      <Split
+        style={{ flex: 1 }}
+        id="radicals-split-container"
+        sizes={[75, 25]}
+        minSize={isLandscape || isSafari ? 10 : 60} // 56px + 4
+        expandToMin={false}
+        gutterSize={10}
+        gutterAlign="center"
+        snapOffset={0}
+        dragInterval={1}
+        direction={"vertical"}
+        cursor="col-resize"
+      >
+        <StrokesRadicalsContainer id={"strokes-radicals-container"}>
+          <StrokesScrollContainer id={"strokes-scroll-container"}>
+            {ls.map((count) => (
+              <div
+                style={{
+                  fontWeight: "bold",
+                  textAlign: "center",
+                  paddingTop: "10px",
+                  paddingBottom: "10px",
+                  cursor: "pointer",
+                }}
+              >
+                {count}
+              </div>
+            ))}
+          </StrokesScrollContainer>
 
-      {/* <ResizePanel direction="n">
-        <div style={{ backgroundColor: "blue", flexGrow: 1 }}>blah</div>
-      </ResizePanel> */}
+          <RadicalsScrollContainer id="radicals-scroll-container" />
+        </StrokesRadicalsContainer>
+
+        <ReadingsScrollContainer>blah32</ReadingsScrollContainer>
+      </Split>
     </RadicalsPageContainer>
   );
 }
