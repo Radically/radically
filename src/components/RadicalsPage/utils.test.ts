57da3372e4098c3c5626fb0c3a51d0d2d225914f
Bryan Kok <bryan.wyern1@gmail.com>
2021-02-12 00:48:36 +0800

    Add unit test for arrayifyForReactWindow

--- src/components/RadicalsPage/utils.test.ts
+++ src/components/RadicalsPage/utils.test.ts
@@ -1,4 +1,5 @@
-import { strokeCountToRadicals } from "./utils";
+import { IntlShape } from "react-intl";
+import { strokeCountToRadicals, arrayifyForReactWindow } from "./utils";
 test("stroke count to list of radicals map", () => {
   const baseRadicals = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
 
@@ -9,5 +10,37 @@ test("stroke count to list of radicals map", () => {
     2: ["d", "e", "f"],
     3: ["g", "h", "i"],
     999: [],
+  });
+});
+
+export const mockIntl = ({
+  defaultLocale: "en",
+  formatDate: () => "mock date",
+  formatHTMLMessage: ({ id }: { id: string }) => id,
+  formatMessage: ({ id }: { id: string }) => id,
+  formatNumber: (value: any) => `${value}`,
+  formatPlural: (value: any) => `${value}`,
+  formatTime: (value: any) => `${value}`,
+  formatRelative: (value: any) => `${value}`,
+  now: () => 0,
+} as unknown) as IntlShape;
+
+test("arrayifyForReactWindow", () => {
+  const strokeCountToRadicals = {
+    1: ["a", "b", "c"],
+    3: ["d", "e", "f", "g"],
+    6: ["h", "i"],
+  };
+
+  expect(arrayifyForReactWindow(strokeCountToRadicals, mockIntl)).toEqual({
+    arrayified: [
+      { header: true, name: "strokes" },
+      { header: false, radicals: ["a", "b", "c"] },
+      { header: true, name: "strokes" },
+      { header: false, radicals: ["d", "e", "f", "g"] },
+      { header: true, name: "strokes" },
+      { header: false, radicals: ["h", "i"] },
+    ],
+    strokeCountToStart: { "1": 0, "3": 2, "6": 4 },
   });
 });
