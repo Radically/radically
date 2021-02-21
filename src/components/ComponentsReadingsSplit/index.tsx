ffab77d70ad820e63e73d647226ff9626c920982
Bryan Kok <bryan.wyern1@gmail.com>
2021-02-21 20:19:04 +0800

    Fix FF Android gutter touch sensitivity issue

--- src/components/ComponentsReadingsSplit/index.tsx
+++ src/components/ComponentsReadingsSplit/index.tsx
@@ -1,7 +1,11 @@
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { FunctionComponent } from "react";
 import Split from "react-split";
+import styled from "styled-components";
 
+const StyledSplit = styled(Split)`
+  flex: 1;
+`;
 const ComponentsReadingSplit: FunctionComponent<{ id?: string }> = (props) => {
   const { children, id } = props;
 
@@ -9,21 +13,27 @@ const ComponentsReadingSplit: FunctionComponent<{ id?: string }> = (props) => {
   const isSafari = navigator.vendor.includes("Apple");
 
   return (
-    <Split
-      style={{ flex: 1 }}
+    <StyledSplit
       id={id || "components-split-container"}
       sizes={[60, 40]}
       minSize={isLandscape || isSafari ? 10 : 60} // 56px + 4
       expandToMin={true}
       gutterSize={10}
       gutterAlign="center"
+      gutterStyle={(dimension: any, gutterSize: any, index: any) => ({
+        height: `${gutterSize}px`,
+        position: "relative",
+        "z-index": 9999,
+        "box-sizing": "border-box",
+        "background-clip": "padding-box",
+      })}
       snapOffset={0}
       dragInterval={1}
       direction={"vertical"}
       cursor="col-resize"
     >
       {children}
-    </Split>
+    </StyledSplit>
   );
 };
 
